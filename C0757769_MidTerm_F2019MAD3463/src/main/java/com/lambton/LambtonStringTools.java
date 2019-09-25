6d0ce3cff836a2938673e9d56007035b0e268b73
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-09-25 18:24:49 -0400

    Commit 3

--- C0757769_MidTerm_F2019MAD3463/src/main/java/com/lambton/LambtonStringTools.java
+++ C0757769_MidTerm_F2019MAD3463/src/main/java/com/lambton/LambtonStringTools.java
@@ -1,7 +1,18 @@
 package com.lambton;
 
+
 public class LambtonStringTools {
 
-    
+    public void reverse(String s)
+    {
+        char[] s1= s.toCharArray();
+        int j= s1.length;
+
+        for(int i =j-1; i>=0; i--)
+        {
+            System.out.print(s1[i]);
+        }
+
+    }
 
 }
